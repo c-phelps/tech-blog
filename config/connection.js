d93afbc7115c8cd5ca96f70c01dbc41e5203535a
Chris Phelps <c.j.phelps89@gmail.com>
2024-09-05 22:13:39 -0400

    fixed a typo created while fixing a typo

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.env.DB_URL)
+  ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: "localhost",
       dialect: "postgres",
